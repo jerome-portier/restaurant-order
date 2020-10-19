9958e471d9e3f601bb39191470089bdf28856efb
jeromeportier <jerome.portier@maersk.com>
2020-10-19 12:46:27 +0100

    Adding config needed to make sure we generate the asset-manifest.json file.

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new WebpackAssetsManifest({
+        output: "asset-manifest.json",
+        customize(entry, original, manifest, asset) {
+          return {
+            key: entry.key === "app.js" ? "main.js" : `/${entry.key}`,
+            value: `/${entry.value}`
+          };
+        }
+      })
+    ],
+    devServer: {
+      headers: {
+        'Access-Control-Allow-Origin': '*'            
+      }
+    }
+    // With the configuration below we rename the filename in the 'value', not the key!?
+    // output: {
+    //   filename: 'main.js'
+    // }
+  }
+};
